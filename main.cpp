69f65d889f4f1058537214f86d77b3389a183dc3
ShadoWing1 <oyungemer14@gmail.com>
2022-11-29 18:53:33 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -21,32 +21,43 @@ int kombinasyon (int sayi1, int sayi2)
     return permutasyon(sayi1,sayi2) / faktoriyel(sayi2);
 }
 
-int main(int argc, char** argv)
+int main()
 {
-    if(argc > 1)
-    {
-        switch (argv[1][0])
-        {
-        case 'f':
-            std::cout << faktoriyel(std::atoi(argv[2])) << '\n';
-            break;
+int sayi1;
+int sayi2;
+int control;
+std::cout << "Fonksiyon = 1 | Permutasyon = 2 | Kombinasyon = 3" << "\n" << ": ";
+std::cout <<"sayi1: ";
+std::cin >> control;
 
-        case 'c':
-            std::cout << kombinasyon(std::atoi(argv[2]), std::atoi(argv[3])) << '\n';
-            break;
+switch (control)
+{
+    case 1:
+        std::cout << "sayi girin \n" << ": ";
+        std::cin >> sayi1; 
+        std::cout <<sayi1 << "! = " << faktoriyel(sayi1) << "\n";
+        break;
+    
+    case 2:
+    std::cout << "1.sayiyi girin \n" << ": ";
+    std::cin >> sayi1; 
+    std::cout << "2.sayiyi girin \n" << ": ";
+    std::cin >> sayi2;
+    std::cout << "P(" << sayi2 << ") = " << permutasyon(sayi1,sayi2) << "\n"; 
+        break;
 
-        case 'p':
-            std::cout << permutasyon(std::atoi(argv[2]), std::atoi(argv[3])) << '\n';
-            break;
+    case 3:
+    std::cout << "1.sayiyi girin \n" << ": ";
+    std::cin >> sayi1; 
+    std::cout << "2.sayiyi girin \n" << ": ";
+    std::cin >> sayi2;
+    std::cout << "C(" << sayi2 << ") = " << kombinasyon(sayi1,sayi2) << "\n"; 
+    break;
 
-        case 'p':
-            std::cout << permutasyon(std::atoi(argv[2]), std::atoi(argv[3])) << '\n';
-            break;
-            
-        default:
-            std::cout << "Bilinmeyen komut\n";
-            break;
-        }
-    }
+
+    default:
+    break;
+}
+
     return 0;
 }
